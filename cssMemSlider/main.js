9ece9d7d8d05bd368f3166f7725d2b18b8feeef8
Evgeniy <epetrovskij35@gmail.com>
2022-05-11 10:53:40 +0300

    feat: add mems (Wedn, May 11, 2022 10:53:24 PM)

---
+++ cssMemSlider/main.js
@@ -0,0 +1,15 @@
+import mems from "./slide.json" assert { type: "json" };
+
+const btnWrapper = document.querySelector(".button__wrapper");
+const container = document.querySelector(".container");
+let idBtn = "1";
+btnWrapper.addEventListener("click", (e) => {
+  console.log(e.target.id);
+});
+
+window.addEventListener("click", (e) => {
+  if (idBtn === "1") {
+    container.innerHTML = `<img src=${mems[e.target.id - 1].img} alt=""/>
+        <p>${mems[e.target.id - 1].text}</p>`;
+  }
+});
